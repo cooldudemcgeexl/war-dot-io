593056f8225a4e8dfcec681dc1ca593dc3699e16
Nick Lay <layns@mail.uc.edu>
2022-11-13 23:38:50 -0500

    restructure, add packages and config

--- models/utils/processing/resizeCrop.py
+++ models/utils/processing/resizeCrop.py
@@ -1,16 +1,15 @@
-from PIL import Image 
-import cv2 #open cv
-
+import cv2  # open cv
+from PIL import Image
 
 if __name__ == "__main__":
 
-    leftCrop   = 0
-    topCrop    = 0
-    rightCrop  = 0
+    leftCrop = 0
+    topCrop = 0
+    rightCrop = 0
     bottomCrop = 0
-    openStr    = ""
-    writeStr   = ""
-    
+    openStr = ""
+    writeStr = ""
+
     img = Image.open("")
     img_cropped = img.crop(leftCrop, topCrop, rightCrop, bottomCrop)
     cv2.imwrite("", img_cropped)
