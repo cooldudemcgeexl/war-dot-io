9760c7c63443f661b48b5fbde08d259e6a61bef2
carl3300 <davenpra@mail.uc.edu>
2023-04-01 18:08:11 -0400

    reconfigured to work without DB

--- backend-layer/main.py
+++ backend-layer/main.py
@@ -1,39 +1,22 @@
-# import sys
-# sys.path.append(r"D:\\Senior Project\\war-dot-io\\backend-layer")
-from .DataContext.Entities.entity import Session, Engine, Base
-from .DataContext.Entities.img_entity import Image, ImageSchema
 from flask import Flask, jsonify, request
-
-Base.metadata.create_all(Engine)
 
 # Start new app
 app = Flask(__name__)
 
 
 @app.route("/images", methods=["GET"])
-def getAllImages():
+def getCurrentImage():
     # Fetch Data
-    session = Session()
-    image_objects = session.query(Image).all()
-
-    # transform into JSON
-    schema = ImageSchema(many=True)
-    images = schema.dump(image_objects)
-    session.close()
-    return jsonify(images)
+    return "Success", 201
 
 
-@app.route("/images", methods=["POST"])
-def addImages():
-    # posted data
-    posted_image = ImageSchema(only=("img_src:", "game_name:")).load(request.get_json())
-    image = Image(**posted_image)
+@app.route("/graphs", methods=["GET"])
+def getGraphs():
+    return "Success", 201
 
-    session = Session()
-    session.add(image)
-    session.commit()
-    session.close()
 
+@app.route("/prediction", methods=["GET"])
+def getPrediction():
     return "Success", 201
 
 
