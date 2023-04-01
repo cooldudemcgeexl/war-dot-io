a2b947fa832bb6db6fb42e0daf51ad4687a9ade1
glonke <nieberdingg@gmail.com>
2023-04-01 18:39:30 -0400

    add backend code

---
+++ backend/Charts.py
@@ -0,0 +1,32 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+"""
+Data
+"""
+correct = 5
+incorrect = 10
+running = True
+
+"""
+Plotting
+"""
+#poll predResult
+predResult = True
+
+fig, ax = plt.subplots()
+bar_colors = ['tab:green', 'tab:red']
+options = ["Correct", "Incorrect"]
+counts = [correct, incorrect]
+ax.set_ylabel('# Predictions')
+ax.set_xlabel('Result')
+
+requested = True
+while running == True:
+    if requested:
+        if predResult == True:
+            correct += 1
+        else:
+            incorrect += 1
+        ax.bar(options, counts, color=bar_colors)
+        plt.show()
