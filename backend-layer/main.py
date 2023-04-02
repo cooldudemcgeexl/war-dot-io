6d3e86d66ef050966c2dc352c5b132b7feb4ebdf
carl3300 <davenpra@mail.uc.edu>
2023-04-01 22:37:24 -0400

    BFF working need to fix base64 encode

--- backend-layer/main.py
+++ backend-layer/main.py
@@ -1,26 +1,28 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from .Services.imageservice import getImage
 from .Services.graphservice import generateNewGraph
 
 # Start new app
 app = Flask(__name__)
+CORS(app)
 
 
-@app.route("/images", methods=["GET"])
+@app.route("/image", methods=["GET"])
 def getCurrentImage():
     img_str = getImage()
-    return img_str, 201
+    return jsonify(img_str), 201
 
 
-@app.route("/graphs", methods=["GET"])
+@app.route("/graph", methods=["GET"])
 def getGraphs():
     graph_str = generateNewGraph()
-    return graph_str, 201
+    return jsonify(graph_str), 201
 
 
 @app.route("/prediction", methods=["GET"])
 def getPrediction():
-    return "Success", 201
+    return jsonify("Success"), 201
 
 
 if __name__ == "__main__":
