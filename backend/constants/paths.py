161838fa24722593a208ff9c0d59a3742b75af30
Nick Lay <layns@mail.uc.edu>
2023-04-05 18:23:36 -0400

    hook up model to backend

--- backend/constants/paths.py
+++ backend/constants/paths.py
@@ -2,9 +2,8 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).parents[2]
 
-MODEL_SAVE_DIR = BASE_DIR / "resources/models"
+MODEL_DIR = BASE_DIR / "resources/models"
 
-MODEL_PATH = BASE_DIR / "resources/models"
 
 IMAGE_DIR = BASE_DIR / "resources/testImages"
 IMAGE_PATH = IMAGE_DIR / "test.png"
--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,14 @@
+from constants.game_list import GAMES
+from constants.paths import IMAGE_PATH, MODEL_DIR
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from .services.graph_service import generate_new_graph
+from model_gen import ModelRunner
+from services.graph_service import generate_new_graph
 
 # Start new app
 app = Flask(__name__)
 CORS(app)
+model_runner = ModelRunner(MODEL_DIR, image_path=IMAGE_PATH, category_list=GAMES)
 
 
 @app.route("/graph", methods=["GET"])
@@ -15,7 +19,8 @@ def getGraphs():
 
 @app.route("/prediction", methods=["GET"])
 def getPrediction():
-    return jsonify("Success"), 201
+    prediction = model_runner.predict(IMAGE_PATH)
+    return jsonify(prediction), 201
 
 
 if __name__ == "__main__":
