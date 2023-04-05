79b1036c972c567bd191036a613c08c6a72caae4
carl3300 <davenpra@mail.uc.edu>
2023-04-04 21:38:50 -0400

    fix two web dirs

--- backend-layer/main.py
+++ backend-layer/main.py
@@ -1,17 +1,10 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from .Services.imageservice import getImage
 from .Services.graphservice import generateNewGraph
 
 # Start new app
 app = Flask(__name__)
 CORS(app)
-
-
-@app.route("/image", methods=["GET"])
-def getCurrentImage():
-    img_str = getImage()
-    return jsonify(img_str), 201
 
 
 @app.route("/graph", methods=["GET"])
