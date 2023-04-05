b302fa8f91df3b82c272e1b4584c62732d24423c
Nick Lay <layns@mail.uc.edu>
2023-04-05 19:35:15 -0400

    fix path whoopsies

--- backend/constants/paths.py
+++ backend/constants/paths.py
@@ -2,10 +2,10 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).parents[2]
 
-MODEL_DIR = BASE_DIR / "resources/models"
+MODEL_SAVE_DIR = BASE_DIR / "resources/models"
 
 
 IMAGE_DIR = BASE_DIR / "resources/testImages"
-IMAGE_PATH = IMAGE_DIR / "test.png"
+IMAGE_PATH = IMAGE_DIR / "image.png"
 
 TRAINING_DATA = BASE_DIR / "resources/dumps/Intro"
