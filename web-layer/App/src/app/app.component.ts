df018320dacc04803f2f80a8688dfabd301b0008
Nick Lay <layns@mail.uc.edu>
2023-04-05 23:48:53 -0400

    change observable type to expose data from backend

--- web-layer/App/src/app/app.component.ts
+++ web-layer/App/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Observable, interval, mergeMap } from 'rxjs';
 import { BACKEND_URL, GRAPH_PATH, IMAGE_PATH } from 'src/constants';
-
+import { PredictionResponse } from 'src/types/predectionResponse';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -22,11 +22,11 @@ export class AppComponent implements OnInit {
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.reloadData().subscribe((x) => (this.outcome = x));
+    this.reloadData().subscribe((x) => (this.outcome = x.game));
     interval(3 * 60 * 1000)
       .pipe(mergeMap(() => this.reloadData()))
       .subscribe((x) => {
-        this.outcome = x;
+        this.outcome = x.game;
         this.timeStamp1 = 1;
         this.timeStamp2 = 1;
       });
@@ -49,7 +49,7 @@ export class AppComponent implements OnInit {
     return this.graph;
   }
 
-  private reloadData(): Observable<string> {
-    return this.http.get<string>(this.backendUrl + '/reload');
+  private reloadData(): Observable<PredictionResponse> {
+    return this.http.get<PredictionResponse>(this.backendUrl + '/reload');
   }
 }
