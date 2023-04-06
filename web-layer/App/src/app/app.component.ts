9439c70154a8e363ab3818150f43f7bdf779181f
Nick Lay <layns@mail.uc.edu>
2023-04-05 22:46:42 -0400

    adjust paths and pull them out into constants

--- web-layer/App/src/app/app.component.ts
+++ web-layer/App/src/app/app.component.ts
@@ -1,59 +1,55 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable, interval, mergeMap } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { timer } from 'rxjs';
-
+import { Component, OnInit } from '@angular/core';
+import { Observable, interval, mergeMap } from 'rxjs';
+import { BACKEND_URL, GRAPH_PATH, IMAGE_PATH } from 'src/constants';
 
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.scss']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
-
 export class AppComponent implements OnInit {
-	public done: boolean = false;
-	public graph: string = "\\wsl$\Ubuntu\home\carl3300\war-dot-io\resources\testImages";
-	public image: string = "assets/flowdiagram.png";
-	public outcome: string = "";
-	private tempImg: string = ""
-	private tempGraph: string = ""
-	private timeStamp1: number = 0;
-	private timeStamp2: number = 0;
-	private url: string = "http://127.0.0.1:8000/";
+  public done: boolean = false;
+  public graph: string = GRAPH_PATH;
+  public image: string = IMAGE_PATH;
+  public outcome: string = '';
+  private tempImg: string = '';
+  private tempGraph: string = '';
+  private timeStamp1: number = 0;
+  private timeStamp2: number = 0;
+  private backendUrl: string = BACKEND_URL;
 
-	constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-	ngOnInit(): void {
-		this.reloadData().subscribe(x => this.outcome = x);
-		interval(3 * 60 * 1000)
-			.pipe(
-				mergeMap(() => this.reloadData())
-			)
-			.subscribe(x => {
-				this.outcome = x;
-				this.timeStamp1 = 1
-				this.timeStamp2 = 1
-			})
-	}
-	public getImage() {
-		if (this.timeStamp1) {
-			this.tempImg = this.image + '?' + this.timeStamp1;
-			this.timeStamp1 = 0;
-			return this.tempImg;
-		}
-		return this.image;
-	}
+  ngOnInit(): void {
+    this.reloadData().subscribe((x) => (this.outcome = x));
+    interval(3 * 60 * 1000)
+      .pipe(mergeMap(() => this.reloadData()))
+      .subscribe((x) => {
+        this.outcome = x;
+        this.timeStamp1 = 1;
+        this.timeStamp2 = 1;
+      });
+  }
+  public getImage() {
+    if (this.timeStamp1) {
+      this.tempImg = this.image + '?' + this.timeStamp1;
+      this.timeStamp1 = 0;
+      return this.tempImg;
+    }
+    return this.image;
+  }
 
-	public getGraph() {
-		if (this.timeStamp2) {
-			this.tempGraph = this.graph + '?' + this.timeStamp2;
-			this.timeStamp2 = 0
-			return this.tempGraph;
-		}
-		return this.graph;
-	}
+  public getGraph() {
+    if (this.timeStamp2) {
+      this.tempGraph = this.graph + '?' + this.timeStamp2;
+      this.timeStamp2 = 0;
+      return this.tempGraph;
+    }
+    return this.graph;
+  }
 
-	private reloadData(): Observable<string> {
-		return this.http.get<string>(this.url + "/reload")
-	}
+  private reloadData(): Observable<string> {
+    return this.http.get<string>(this.backendUrl + '/reload');
+  }
 }
---
+++ web-layer/App/src/constants.ts
@@ -0,0 +1,4 @@
+export const RESOURCE_DIR = 'assets/testImages';
+export const IMAGE_PATH = RESOURCE_DIR + '/image.png';
+export const GRAPH_PATH = RESOURCE_DIR + '/graph.png';
+export const BACKEND_URL = 'http://127.0.0.1:5000/';
