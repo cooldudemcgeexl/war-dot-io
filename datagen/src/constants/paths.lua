65cf0336451ec1d03181c7b9cabd36a56c02b1ba
Nick Lay <layns@mail.uc.edu>
2023-04-06 00:32:14 -0400

    also output the game title to game.txt

--- datagen/src/constants/paths.lua
+++ datagen/src/constants/paths.lua
@@ -1,7 +1,9 @@
 local screenshotDir = 'C:\\Users\\asill\\OneDrive\\Documents\\repos\\wardotio\\resources\\dumps'
+local demoDir = "\\\\wsl.localhost\\Ubuntu\\home\\nick\\repos\\war-dot-io\\resources\\testImages"
 
 local paths = {
-    screenshotDir = screenshotDir
+    screenshotDir = screenshotDir,
+    demoDir = demoDir
 }
 
 return paths
--- datagen/src/runtime.lua
+++ datagen/src/runtime.lua
@@ -1,21 +1,58 @@
-local outputDir = "\\\\wsl.localhost\\Ubuntu\\home\\nick\\repos\\war-dot-io\\resources\\testImages\\"
-local fileName = 'image.png'
-local outputPath = outputDir .. fileName
+local gameDetector  = require('utils.detectGame')
+local vram          = require('utils.vram')
+local fileWriter    = require('utils.writeFile')
+local paths         = require('constants.paths')
+local imageFile     = '\\image.png'
+local textFile      = '\\game.txt'
+local imagePath     = paths.demoDir .. imageFile
+local textPath      = paths.demoDir .. textFile
 local frameInterval = 5
 
 
-function printGameInfo()
+local function printGameInfo()
     console:log(emu:getGameCode())
 end
 
-function writeFrame()
+local function writeFrame()
     local currFrame = emu:currentFrame()
     if currFrame % frameInterval == 0 then
         console:log('Writing frame\n')
-        console:log(outputPath)
-        emu:screenshot(outputDir .. fileName)
+        console:log(imagePath)
+        emu:screenshot(imagePath)
+    end
+end
+
+DetectThisFrame = false
+ActiveGame = 'None'
+ObjVRAMStart = 'NONE'
+
+local function detectCurrentGame()
+    if DetectThisFrame then
+        local currentGame = gameDetector.detectGame()
+        if currentGame ~= "None" then
+            DetectThisFrame = false
+            FramesThisGame = 1
+        end
+        ActiveGame = currentGame
+    else
+        local newObjVRAM = vram.hasOBJVRAMChanged(ObjVRAMStart)
+        if newObjVRAM ~= nil then
+            console:log("CHANGED")
+            ObjVRAMStart = newObjVRAM
+            DetectThisFrame = true
+        end
+    end
+end
+
+local function writeCurrentGame()
+    local currFrame = emu:currentFrame()
+    if currFrame % frameInterval == 0 then
+        console:log("Current game: " .. ActiveGame)
+        fileWriter.writeToTextFile(textPath, ActiveGame)
     end
 end
 
 callbacks:add('startup', printGameInfo)
 callbacks:add('frame', writeFrame)
+callbacks:add('frame', detectCurrentGame)
+callbacks:add('frame', writeCurrentGame)
